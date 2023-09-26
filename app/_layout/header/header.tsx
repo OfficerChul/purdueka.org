a31f0cd8b6039ee8aeb0188cb0e82a45e863de23
OfficerChul <gcj1234567890@gmail.com>
2023-09-26 19:07:10 -0400

    header: make underline effect variable

--- app/_layout/header/header.tsx
+++ app/_layout/header/header.tsx
@@ -19,12 +19,12 @@ export default function Header() {
           <button className='' onClick={toggleMenu}>{x_btn}</button>
 
           <ul className="text-yellow-600 py-2 px-2md:hidden mobile-menu pr-24">
-            <li><a href='/intro' className=" text-2xl relative w-fit after:block after:content-[''] after:absolute after:h-[1px] after:bg-yellow-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">소개</a></li>
-            <li><a href='/info' className=" text-2xl relative w-fit after:block after:content-[''] after:absolute after:h-[1px] after:bg-yellow-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">퍼듀 정보</a></li>
-            <li><a href='/ann' className=" text-2xl relative w-fit after:block after:content-[''] after:absolute after:h-[1px] after:bg-yellow-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">채용 공고</a></li>
-            <li><a href='/contact' className=" text-2xl relative w-fit after:block after:content-[''] after:absolute after:h-[1px] after:bg-yellow-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">Contact Us</a></li>
-            <li><a href='/search' className=" text-2xl relative w-fit after:block after:content-[''] after:absolute after:h-[1px] after:bg-yellow-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">검색</a></li>
-            <li><a href='/login' className=" text-2xl relative w-fit after:block after:content-[''] after:absolute after:h-[1px] after:bg-yellow-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">로그인</a></li>
+            <li><a href='/intro' className={`${underlineEffect} text-2xl`}>소개</a></li>
+            <li><a href='/info' className={`${underlineEffect} text-2xl`}>퍼듀 정보</a></li>
+            <li><a href='/ann' className= {`${underlineEffect} text-2xl`}>채용 공고</a></li>
+            <li><a href='/contact' className= {`${underlineEffect} text-2xl`}>Contact Us</a></li>
+            <li><a href='/search' className= {`${underlineEffect} text-2xl`}>검색</a></li>
+            <li><a href='/login' className= {`${underlineEffect} text-2xl`}>로그인</a></li>
 
           </ul>
         </div>
@@ -41,12 +41,12 @@ export default function Header() {
           <div className="grow"></div>
           <ul className="text-yellow-600 md:flex gap-10 hidden">
 
-            <li><a href='/intro' className="relative w-fit after:block after:content-[''] after:absolute after:h-[1px] after:bg-yellow-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">소개</a></li>
-            <li><a href='/info' className="relative w-fit after:block after:content-[''] after:absolute after:h-[1px] after:bg-yellow-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">퍼듀 정보</a></li>
-            <li><a href='/ann' className="relative w-fit after:block after:content-[''] after:absolute after:h-[1px] after:bg-yellow-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">채용 공고</a></li>
-            <li><a href='/contact' className="relative w-fit after:block after:content-[''] after:absolute after:h-[1px] after:bg-yellow-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">Contact Us</a></li>
-            <li><a href='/search' className="relative w-fit after:block after:content-[''] after:absolute after:h-[1px] after:bg-yellow-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">검색</a></li>
-            <li><a href='/login' className="relative w-fit after:block after:content-[''] after:absolute after:h-[1px] after:bg-yellow-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left">로그인</a></li>
+            <li><a href='/intro' className={`${underlineEffect}`}>소개</a></li>
+            <li><a href='/info' className={`${underlineEffect}`}>퍼듀 정보</a></li>
+            <li><a href='/ann' className={`${underlineEffect}`}>채용 공고</a></li>
+            <li><a href='/contact' className={`${underlineEffect}`}>Contact Us</a></li>
+            <li><a href='/search' className={`${underlineEffect}`}>검색</a></li>
+            <li><a href='/login' className={`${underlineEffect}`}>로그인</a></li>
 
           </ul>
 
@@ -79,3 +79,5 @@ const hamburger = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0
 const x_btn = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
 <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
 </svg>
+
+const underlineEffect = "relative w-fit after:block after:content-[''] after:absolute after:h-[1px] after:bg-yellow-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left"
