650a4c6de9628f59ca8d77eef6472cbf5fe8c173
OfficerChul <gcj1234567890@gmail.com>
2023-09-26 18:14:19 -0400

    header: fix className

--- app/_layout/header/header.tsx
+++ app/_layout/header/header.tsx
@@ -16,7 +16,7 @@ export default function Header() {
     <>
       <div className={`mobile-menu ${isMenuOpen ? '' : 'hidden'} bg-yellow-600 fixed h-full right-0`}>
         <div className="bg-[#CEB888] h-full mt-1 mb-1">
-          <button className='' onClick={toggleMenu}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className ="w-6 h-6">
+          <button className='' onClick={toggleMenu}><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
           </svg></button>
 
