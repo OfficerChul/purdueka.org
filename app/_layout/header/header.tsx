d0c22d9cedb2b0089a26b4e9b8d4f5cc573b3ce1
OfficerChul <gcj1234567890@gmail.com>
2023-10-01 00:40:05 -0400

    header: mobile sliding effect

--- app/_layout/header/header.tsx
+++ app/_layout/header/header.tsx
@@ -14,17 +14,19 @@ export default function Header() {
 
   return (
     <>
-      <div className={`mobile-menu ${isMenuOpen ? '' : 'hidden'} bg-yellow-600 fixed h-full right-0`}>
-        <div className="bg-[#CEB888] h-full mt-1 mb-1">
-          <button className='' onClick={toggleMenu}>{x_btn}</button>
+
+      <div className={`py-2 transition-all duration-300 ease-in ${isMenuOpen ? "" : 'right-[-100%]'} bg-yellow-600 fixed h-full right-0 w-9/12`}>
+
+        <div className="bg-[#CEB888] h-full p-5">
+          
           {/* mobile screen */}
-          <ul className="text-yellow-600 py-2 px-2 md:hidden mobile-menu pr-24">
+          <ul className="text-yellow-600">
             <li><a href='/intro' className={`${underlineEffect} text-2xl`}>소개</a></li>
             <li><a href='/info' className={`${underlineEffect} text-2xl`}>퍼듀 정보</a></li>
-            <li><a href='/ann' className= {`${underlineEffect} text-2xl`}>채용 공고</a></li>
-            <li><a href='/contact' className= {`${underlineEffect} text-2xl`}>Contact Us</a></li>
-            <li><a href='/search' className= {`${underlineEffect} text-2xl`}>검색</a></li>
-            <li><a href='/login' className= {`${underlineEffect} text-2xl`}>로그인</a></li>
+            <li><a href='/ann' className={`${underlineEffect} text-2xl`}>채용 공고</a></li>
+            <li><a href='/contact' className={`${underlineEffect} text-2xl`}>Contact Us</a></li>
+            <li><a href='/search' className={`${underlineEffect} text-2xl`}>검색</a></li>
+            <li><a href='/login' className={`${underlineEffect} text-2xl`}>로그인</a></li>
 
           </ul>
         </div>
@@ -33,7 +35,12 @@ export default function Header() {
       <nav className=" bg-stone-950 w-full h-20">
         <div className="items-center m-auto container flex justify-between truncate">
 
-
+          <button onClick={toggleMenu} className={`${isMenuOpen ? "hidden" : ""} text-yellow-600 md:hidden absolute right-5`}>
+            {hamburger}
+          </button>
+          <button onClick={toggleMenu} className={`${isMenuOpen ? "" : "hidden"} text-yellow-600 md:hidden absolute right-5`}>
+            {close}
+          </button>
 
           <Image className='h-20 m-auto' src='/pka_logo.png' alt='logo' width='317' height='96'></Image>
 
@@ -50,9 +57,7 @@ export default function Header() {
 
           </ul>
 
-          <button onClick={toggleMenu} className='text-yellow-600 pr-3 md:hidden mobile-menu-button'>
-            {hamburger}
-          </button>
+
 
 
 
@@ -61,16 +66,24 @@ export default function Header() {
 
 
       </nav>
+
+
+      {/* banner start */}
+
+      <div className="h-[36vw] truncate">
+        <Image alt='banner' src='/banner.jpeg' className='w-full' width='1280' height='1280'></Image>
+      </div>
     </>
   )
 }
 
 const hamburger = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-<path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+  <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
 </svg>
 
-const x_btn = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
-<path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+const close = <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
 </svg>
+
 
 const underlineEffect = "relative w-fit after:block after:content-[''] after:absolute after:h-[1px] after:bg-yellow-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-left"
