28dc2af3641dde2bd4098a2f5c2c248b46653c45
chowder985 <ilhoonlee1@gmail.com>
2023-10-23 16:24:49 -0400

    Miniboard: add limit to number of posts

--- app/_components/miniboard/miniboard.tsx
+++ app/_components/miniboard/miniboard.tsx
@@ -5,24 +5,28 @@
 import React from 'react'
 import { BoardResponseDTO } from '../../_dto/board.dto'
 import { Category } from 'app/_shared/enums/categories';
+import { paginate } from 'app/_helper/pagination';
 
 export default function Miniboard(props: { category: string, data: Array<BoardResponseDTO> }) {
   const { category, data } = props;
   const categoryKor = Object.values(Category)[Object.keys(Category).indexOf(category)]
+  const numOfPosts = 10;
 
   const OnPostClick = (event: any, id: any) => {
     window.open("/announcement/"+id, '_blank')?.focus()
   }
 
+  const paginatedPosts = paginate(data, 1, numOfPosts);
+
   return (
-    <div className="flex justify-center bg-yellow-600 h-72">
+    <div className="flex justify-center bg-yellow-600 h-fit">
       <div className="flex overflow-hidden flex-col text-black border-8 border-white w-full mt-1 mb-1 bg-white">
         <div className="flex justify-between mb-4">
           <span className="text-xl">{categoryKor}</span>
           <a className='text-xs' href={'/'+category}>더보기</a>
         </div>
         {
-          data.map( 
+          paginatedPosts.map( 
             e => <div key={e.id} className='flex justify-between cursor-pointer hover:text-stone-400' onClick={event => OnPostClick(event, e.id)}>
               <span>{e.title}</span>
               <span className='text-stone-400 text-xs'>{e.date}</span>
