acd7d963f3f517b9f1de29b00cc8e9f4f80280e6
chowder985 <ilhoonlee1@gmail.com>
2023-11-07 00:06:47 -0500

    Pagination: moved pages array logic to helper

--- app/_components/pagination/pagination.tsx
+++ app/_components/pagination/pagination.tsx
@@ -1,4 +1,4 @@
-import { Key } from "react";
+import { generatePageIndexArray } from "app/_helper/pagination";
 
 export default function Pagination (props: { items: number, pageSize: number, currentPage: number, OnPageChange: any }) {
     const { items, pageSize, currentPage, OnPageChange } = props;
@@ -10,10 +10,7 @@ export default function Pagination (props: { items: number, pageSize: number, cu
         return null;
     }
 
-    let pages: Key[] = [];
-    for (let i=1; i<=pagesCount; i++) {
-        pages.push(i);
-    }
+    const pages = generatePageIndexArray(pagesCount);
 
     let prevBtn = <a className='pe-2 text-yellow-600 hover:text-black cursor-pointer' onClick={() => OnPageChange(currentPage - 1)}>&lt; Prev</a>;
     let nextBtn = <a className='ps-2 text-yellow-600 hover:text-black cursor-pointer' onClick={() => OnPageChange(currentPage + 1)}>Next &gt;</a>;
--- app/_helper/pagination.tsx
+++ app/_helper/pagination.tsx
@@ -1,6 +1,16 @@
+import { Key } from 'react';
 import { BoardResponseDTO } from '../_dto/board.dto'
 
 export const paginate = (data: BoardResponseDTO[], pageNum: number, pageSize: number) => {
     const startIndex = (pageNum - 1) * pageSize;
     return data.slice(startIndex, startIndex + pageSize);
+}
+
+export const generatePageIndexArray = (pagesCount: number) => {
+    let pages: Key[] = [];
+    for (let i=1; i<=pagesCount; i++) {
+        pages.push(i);
+    }
+
+    return pages;
 }
