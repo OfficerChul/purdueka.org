c7f41f2b5fa810d059214762dc7b524d19707f3c
OfficerChul <gcj1234567890@gmail.com>
2023-11-22 18:31:38 -0500

    sponsor data: added sponsor data

---
+++ app/_shared/tests/sponsor_data.tsx
@@ -0,0 +1,43 @@
+const data = [
+    {
+        id: 0,
+        img: "/pka_logo.png",
+        type: "platinum"
+    },
+    {
+        id: 0,
+        img: "/pka_logo.png",
+        type: "platinum"
+    },
+    {
+        id: 0,
+        img: "/pka_logo.png",
+        type: "gold"
+    },
+    {
+        id: 0,
+        img: "/pka_logo.png",
+        type: "gold"
+    },
+    {
+        id: 0,
+        img: "/pka_logo.png",
+        type: "gold"
+    },
+    {
+        id: 0,
+        img: "/pka_logo.png",
+        type: "silver"
+    },
+    {
+        id: 0,
+        img: "/pka_logo.png",
+        type: "silver"
+    },
+    {
+        id: 0,
+        img: "/pka_logo.png",
+        type: "silver"
+    }
+]
+export default data;
