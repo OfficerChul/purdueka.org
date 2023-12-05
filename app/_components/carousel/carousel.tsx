085606b38d4b1548924591be1e3a52ed42db2cca
OfficerChul <gcj1234567890@gmail.com>
2023-12-05 18:37:02 -0500

    carousel: clean code

--- app/_components/carousel/carousel.tsx
+++ app/_components/carousel/carousel.tsx
@@ -2,9 +2,9 @@
 
 import { Carousel } from 'flowbite-react';
 
-export default function PKACarousel(props: { image_data: any , leftControl: string, rightControl: string, slideInterval: number}) {
+export default function PKACarousel(props: { image_data: any }) {
     return (
-        <Carousel pauseOnHover slide slideInterval={props.slideInterval} leftControl={props.leftControl} rightControl={props.rightControl}>
+        <Carousel pauseOnHover slide slideInterval={3000} leftControl="left" rightControl="right">
             {props.image_data.map((data: any) => <img key={data.key} src={data.url} alt={data.url} width='100' height='100' className='z-20'></img>)}
         </Carousel>
 
