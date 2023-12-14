1a5ce4273b2265acfa64ce0e4443b713e171a23d
OfficerChul <gcj1234567890@gmail.com>
2023-12-14 00:35:43 -0500

    pka_member: create pka member dataset

---
+++ app/_shared/member/pka_member.tsx
@@ -0,0 +1,343 @@
+export const pka_member_data = 
+[
+    {
+        name: "김지우",
+        name_us: "Rachel",
+        major: "Electrical Engineering",
+        email: "kim2999@purdue.edu",
+        role: "회계 팀장",
+        role_us: "Treasurery Manager",
+        image: "김지우.jpeg",
+        team: "treasurer"
+    },
+    {
+        name: "서다윤",
+        name_us: "Dayoon Suh",
+        major: "Data Science",
+        email: "suh65@purdue.edu",
+        role: "회계 부팀장",
+        role_us: "Treasurery Manager",
+        image: "서다윤.jpeg",
+        team: "treasurer"
+    },
+    {
+        name: "홍서연",
+        name_us: "Seoyeon Hong",
+        major: "Doctor of Pharmacy",
+        email: "hong309@purdue.edu",
+        role: "웹 & 미디어 팀장",
+        role_us: "Web Media Manager",
+        image: "홍서연.jpeg",
+        team: "web"
+    },
+    {
+        name: "김성우",
+        name_us: "Sungwoo Kim",
+        major: "Computer Science",
+        email: "kim3583@purdue.edu",
+        role: "웹 & 미디어 부팀장",
+        role_us: "Web Media",
+        image: "김성우.jpeg",
+        team: "web"
+    },
+    {
+        name: "여기수",
+        name_us: "Gisu Yeo",
+        major: "Computer Science",
+        email: "gyeo@purdue.edu",
+        role: "웹 & 미디어 부팀장",
+        role_us: "Web & Media Manager",
+        image: "여기수.png",
+        team: "web"
+    },
+    {
+        name: "지예원",
+        name_us: "Yewon Ji",
+        major: "Professional Flight and CNIT",
+        email: "ji176@purdue.edu",
+        role: "웹 & 미디어",
+        role_us: "Web Media",
+        image: "지예원.JPG",
+        team: "web"
+    },
+    {
+        name: "장교철",
+        name_us: "Kyochul Jang",
+        major: "Computer Science",
+        email: "jang128@purdue.edu",
+        role: "웹 & 미디어",
+        role_us: "Web Media",
+        image: "장교철.jpg",
+        team: "web"
+    },
+    {
+        name: "이일훈",
+        name_us: "Ilhoon Lee",
+        major: "Computer Science",
+        email: "lee3252@purdue.edu",
+        role: "웹 & 미디어",
+        role_us: "Web Media",
+        image: "이일훈.jpg",
+        team: "web"
+    },
+    {
+        name: "이수아",
+        name_us: "Sophia Lee",
+        major: "Industrial Engineering",
+        email: "lee3487@purdue.edu",
+        role: "웹 & 미디어",
+        role_us: "Web Media",
+        image: "이수아.png",
+        team: "web"
+    },
+    {
+        name: "임재형",
+        name_us: "Jaehyung Lim",
+        major: "Doctor of Pharmacy",
+        email: "lim264@purdue.edu",
+        role: "PKA 회장",
+        role_us: "PKA President",
+        image: "임재형.jpeg",
+        team: "presidential"
+    },
+    {
+        name: "이석천",
+        name_us: "Seokcheon Lee",
+        major: "Professor of Industrial Engineering",
+        email: "stonesky@purdue.edu",
+        role: "PKA 지도교수",
+        role_us: "PKA Advisor",
+        image: "이석천.jpeg",
+        team: "presidential"
+    },
+    {
+        name: "강성엽",
+        name_us: "Lt. Song Kang",
+        major: "Purdue Univ. Police Dept.",
+        email: "sykang2@purdue.edu",
+        role: "PKA 대학경찰고문",
+        role_us: "Community Policing Advisor",
+        image: "강성엽.jpeg",
+        team: "presidential"
+    },
+    {
+        name: "위수현",
+        name_us: "Suhyeon We",
+        major: "Doctor of Pharmacy",
+        email: "we@purdue.edu",
+        role: "PKA 부회장",
+        role_us: "PKA Vice President",
+        image: "위수현.png",
+        team: "presidential"
+    },
+    {
+        name: "홍현석",
+        name_us: "Hyunseok Hong",
+        major: "Electrical and Computer Engineering",
+        email: "hong274@purdue.edu",
+        role: "PKA 부회장",
+        role_us: "PKA Vice President",
+        image: "홍현석.png",
+        team: "presidential"
+    },
+    {
+        name: "오명",
+        name_us: "Myoung oh",
+        major: "Computer and information technology",
+        email: "Oh278@purdue.edu",
+        role: "이벤트",
+        role_us: "Event",
+        image: "오명.jpeg",
+        team: "event"
+    },
+    {
+        name: "김준우",
+        name_us: "Daniel Kim",
+        major: "Mechanical Engineering",
+        email: "kim4083@purdue.edu",
+        role: "이벤트",
+        role_us: "Event",
+        image: "김준우.jpeg",
+        team: "event"
+    },
+    {
+        name: "양소민",
+        name_us: "Somin Yang",
+        major: "Finance",
+        email: "yang2339@purdue.edu",
+        role: "이벤트",
+        role_us: "Event",
+        image: "양소민.jpeg",
+        team: "event"
+    },
+    {
+        name: "민지인",
+        name_us: "Jiin Min",
+        major: "Data Science",
+        email: "min100@purdue.edu",
+        role: "이벤트",
+        role_us: "Event",
+        image: "민지인.jpeg",
+        team: "event"
+    },
+    {
+        name: "육도경",
+        name_us: "Peter Yuk",
+        major: "Computer Science",
+        email: "dyuk@purdue.edu",
+        role: "이벤트",
+        role_us: "Event",
+        image: "육도경.jpeg",
+        team: "event"
+    },
+    {
+        name: "송채헌",
+        name_us: "Dennis Sawng",
+        major: "First Year Engineering",
+        email: "csawng@purdue.edu",
+        role: "이벤트",
+        role_us: "Event",
+        image: "송채헌.jpeg",
+        team: "event"
+    },
+    {
+        name: "김유민",
+        name_us: "Yumin Kim",
+        major: "Integrated Business & Engineering",
+        email: "kim3734@purdue.edu",
+        role: "이벤트",
+        role_us: "Event",
+        image: "김유민.jpeg",
+        team: "event"
+    },
+    {
+        name: "조현우",
+        name_us: "Hyunwoo Cho",
+        major: "Civil Engineering",
+        email: "cho422@purdue.edu",
+        role: "이벤트",
+        role_us: "Event",
+        image: "조현우.jpeg",
+        team: "event"
+    },
+    {
+        name: "정유진",
+        name_us: "Yoojin Jeong",
+        major: "Hospitality and Tourism Management",
+        email: "jeong186@purdue.edu",
+        role: "이벤트",
+        role_us: "Event",
+        image: "정유진.jpeg",
+        team: "event"
+    },
+    {
+        name: "오승근",
+        name_us: "Aiden Oh",
+        major: "Computer Engineering",
+        email: "oh183@purdue.edu",
+        role: "리쿠르팅 팀장",
+        role_us: "Recruiting Manager",
+        image: "오승근.jpeg",
+        team: "recruiting"
+    },
+    {
+        name: "김우식",
+        name_us: "Woosik Kim",
+        major: "Computer Engineering",
+        email: "kim3124@purdue.edu",
+        role: "리쿠르팅",
+        role_us: "Recruiting",
+        image: "김우식.jpeg",
+        team: "recruiting"
+    },
+    {
+        name: "윤민채",
+        name_us: "Minchae Yun",
+        major: "Computer Science",
+        email: "yun69@purdue.edu",
+        role: "리쿠르팅",
+        role_us: "Recruiting",
+        image: "윤민채.jpeg",
+        team: "recruiting"
+    },
+    {
+        name: "백진욱",
+        name_us: "Jinwook paek",
+        major: "UAS(unmanned aerial systems)",
+        email: "paekj@purdue.edu",
+        role: "리쿠르팅",
+        role_us: "Recruiting",
+        image: "백진욱.jpeg",
+        team: "recruiting"
+    },
+    {
+        name: "정현식",
+        name_us: "Richard",
+        major: "Construction Management",
+        email: "jung247@purdue.edu",
+        role: "리쿠르팅",
+        role_us: "Recruiting",
+        image: "정현식.jpeg",
+        team: "recruiting"
+    },
+    {
+        name: "고경명",
+        name_us: "Kyung Myung Ko",
+        major: "Computer Science",
+        email: "ko112@purdue.edu",
+        role: "리쿠르팅",
+        role_us: "Recruiting",
+        image: "고경명.jpeg",
+        team: "recruiting"
+    },
+    {
+        name: "김진",
+        name_us: "Gene Kim",
+        major: "Computer Science",
+        email: "kim3115@purdue.edu",
+        role: "리쿠르팅",
+        role_us: "Recruiting",
+        image: "김진.JPG",
+        team: "recruiting"
+    },
+    {
+        name: "장준",
+        name_us: "Jun Jang",
+        major: "PharmD",
+        email: "jang91@purdue.edu",
+        role: "리쿠르팅",
+        role_us: "Recruiting",
+        image: "장준.png",
+        team: "recruiting"
+    },
+    {
+        name: "임해나",
+        name_us: "Hannah Butler",
+        major: "Hospitality & Tourism Management",
+        email: "butle257@purdue.edu",
+        role: "리쿠르팅",
+        role_us: "Recruiting",
+        image: "임해나.jpeg",
+        team: "recruiting"
+    },
+    {
+        name: "최예린",
+        name_us: "Ye Rin Choi",
+        major: "Doctor of Pharmacy",
+        email: "Choi568@purdue.edu",
+        role: "대외협력 및 재정관리 팀장",
+        role_us: "Outsourcing and Financing Manager",
+        image: "최예린.png",
+        team: "outsourcing"
+    },
+    {
+        name: "한정원",
+        name_us: "Jeong Won Han",
+        major: "Doctor of Pharmacyg",
+        email: "han427@purdue.edu",
+        role: "대외협력 및 재정관리 부팀장",
+        role_us: "Outsourcing and Financing Manager",
+        image: "한정원.jpeg",
+        team: "outsourcing"
+    }
+]
