301447040910c132d8eb67e23d521eed309685fa
OfficerChul <gcj1234567890@gmail.com>
2023-12-14 00:34:48 -0500

    carousel: change img to Image

--- app/_components/carousel/carousel.tsx
+++ app/_components/carousel/carousel.tsx
@@ -1,11 +1,12 @@
 'use Client';
 
 import { Carousel } from 'flowbite-react';
+import Image from 'next/image';
 
 export default function PKACarousel(props: { image_data: any }) {
     return (
         <Carousel pauseOnHover slide slideInterval={3000} leftControl="left" rightControl="right">
-            {props.image_data.map((data: any) => <img key={data.key} src={data.url} alt={data.url} width='100' height='100' className='z-20'></img>)}
+            {props.image_data.map((data: any) => <Image key={data.key} src={data.url} alt={data.url} width='100' height='100' className='z-20'></Image>)}
         </Carousel>
 
     )
