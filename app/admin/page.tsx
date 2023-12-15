60ca12ee64b2a95ad61c19e2d1fae4ca256e85de
chowder985 <ilhoonlee1@gmail.com>
2023-12-14 19:36:38 -0500

    Admin: add isLoading

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,10 +1,22 @@
+'use client'
+
 import UserList from "../_components/admin/userlist"
 import { AllUserDataResponseDTO, UserDTO } from "app/_dto/user.dto"
 import api from "app/_api/"
+import { useEffect, useState } from "react"
 
-export default async function Page() {
-    const users: AllUserDataResponseDTO = await api.user.getAllUsers()
-    console.log(users)
+export default function Page() {
+    const [users, setUsers] = useState({} as AllUserDataResponseDTO)
+    const [isLoading, setLoading] = useState(true)
+
+    useEffect(() => {
+        api.user.getAllUsers()
+          .then(e => setUsers(e))
+          .finally(() => setLoading(false))
+      }, [])
+
+    if (isLoading) return <p>Loading...</p>
+    if (!users) return <p>No data</p>
 
     return (
         <div className="mt-20">
